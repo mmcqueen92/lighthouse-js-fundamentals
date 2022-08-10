0e9d147e3e6175cdb172af271e7677d7101061f3
Malcolm McQueen <malcolmmcqueen@live.com>
2022-08-10 05:24:05 +0000

    trying to add all files

---
+++ loopy-lighthouse.js
@@ -0,0 +1,14 @@
+let output = ''
+for (let x = 100; x < 201; x++) {
+  let output = '';
+  if (x % 3 === 0) {
+    output += 'Loopy';
+  }
+  if (x % 4 === 0) {
+    output += 'Lighthouse';
+  }
+  else if (!output) {
+    output += x;
+  }
+  console.log(output);
+}
